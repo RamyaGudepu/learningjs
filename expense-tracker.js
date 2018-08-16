2fce8f894a36ac63e7eac224ccd2eb99be406213
RamyaGudepu <40195665+RamyaGudepu@users.noreply.github.com>
2018-08-16 16:36:20 +0530

    expense-tracker

---
+++ expense-tracker.js
@@ -0,0 +1,20 @@
+const account = {
+    name : 'Andrew Mead',
+    expenses: [],
+    addExpense: function (description,amount) {
+        this.expenses.push({
+            description: description,
+            amount: amount
+        })
+    },
+    getAccountSummary: function(){
+        let totalExpenses = 0
+        this.expenses.forEach(function(expense){
+       totalExpenses = totalExpenses + expense.amount
+        })
+  return `${this.name}has $${totalExpenses} in expenses`
+    }
+}
+account.addExpense('Rent',950)
+account.addExpense('Coffee',2)
+console.log(account.getAccountSummary())
